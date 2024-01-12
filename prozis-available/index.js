70049bc01bc498c6b2f51c95f872cb13bc0a76ba
Arnaiz23 <aaf110501@gmail.com>
2024-01-12 22:05:36 +0100

    feat ✨: Update the prozis script for changes in them web

--- prozis-available/index.js
+++ prozis-available/index.js
@@ -1,32 +1,35 @@
-import { firefox } from "playwright";
+import { firefox } from "playwright"
 
 const URLS = {
-	barbacue: "https://www.prozis.com/es/es/prozis/zero-barbecue-355-g",
-	chocobutter400:
-		"https://www.prozis.com/es/es/prozis/whey-choco-butter-400-g-nutchoc",
-	chocobutter200:
-		"https://www.prozis.com/es/es/prozis/whey-choco-butter-200-g-nutchoc",
-};
-
-async function getStatus(url) {
-	const browser = await firefox.launch({ headless: true, slowMo: 200 });
-	try {
-		const page = await browser.newPage();
-
-		await page.goto(url);
-
-		const state = await page.$eval("#noticeSection", (el) => el.textContent);
-
-		return state;
-	} catch (err) {
-		await getStatus(url);
-		// return null;
-	} finally {
-		await browser.close();
-	}
+  barbacue: "https://www.prozis.com/es/es/prozis/zero-barbecue-355-g",
+  chocobutter400:
+    "https://www.prozis.com/es/es/prozis/whey-choco-butter-400-g-nutchoc",
+  chocobutter200:
+    "https://www.prozis.com/es/es/prozis/whey-choco-butter-200-g-nutchoc",
 }
+
+async function getStatus(url, page) {
+  try {
+    await page.goto(url)
+
+    await page.waitForSelector(".stock-info")
+    const state = await page.$eval(".stock-info", (el) => el.textContent)
+
+    return state
+  } catch (err) {
+    console.error("Errror: " + err)
+    await getStatus(url, page)
+  }
+}
+
+console.log("Initializing the script!!! 🚀🚀🚀")
+
+const browser = await firefox.launch({ headless: true, slowMo: 200 })
+const page = await browser.newPage()
 
 for (const url in URLS) {
-	const state = await getStatus(URLS[url]);
-	console.log(`State ${url}: ${state}`);
+  const state = await getStatus(URLS[url], page)
+  console.log(`State ${url}: ${state}`)
 }
+
+await browser.close()
